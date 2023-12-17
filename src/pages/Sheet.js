d93cb38a3e0459ec32b10c5f22d519c58ad31018
diegoTrueZenit <diego@truezenithcreative.com>
2023-12-17 15:38:10 -0800

    .

--- src/pages/Sheet.js
+++ src/pages/Sheet.js
@@ -87,7 +87,7 @@ function Sheet() {
              <td className='description'> <i className="bi bi-square"></i> 6’H X 4’W ALUM H.D. FRAMES </td>
              <td> {elements.af6x4 > 0 ? elements.af6x4 : ""} </td>
              <td></td>
-             <td className='description'> NO. O SHORES 3' - 4'6" (GREEN) </td>
+             <td className='description'> NO. 0 SHORES 3' - 4'6" (GREEN) </td>
              <td> {elements.sh03 > 0 ? elements.sh03 : ""} </td>
              <td></td>
             </tr>
@@ -95,7 +95,7 @@ function Sheet() {
              <td className='description'> <i className="bi bi-square"></i> 5’H X 4’W ALUM H.D. FRAMES </td>
              <td> {elements.af5x4 > 0 ? elements.af5x4 : ""} </td>
              <td></td>
-             <td className='description'> NO. O SHORES 4' - 6'6" (SILVER)</td>
+             <td className='description'> NO. 0 SHORES 4' - 6'6" (SILVER)</td>
              <td> {elements.sh04 > 0 ? elements.sh04 : ""} </td>
              <td></td>
             </tr>
@@ -103,7 +103,7 @@ function Sheet() {
              <td className='description'> <i className="bi bi-square"></i> 4’H X 4’W ALUM H.D. FRAMES </td>
              <td> {elements.af4x4 > 0 ? elements.af4x4 : ""} </td>
              <td></td>
-             <td className='description'> NO. O SHORES 3'9" - 6' (GREEN) </td>
+             <td className='description'> NO. 0 SHORES 3'9" - 6' (GREEN) </td>
              <td> {elements.sh039 > 0 ? elements.sh039 : ""} </td>
              <td></td>
             </tr>
@@ -164,7 +164,7 @@ function Sheet() {
              <td></td>
           </tr>
           <tr>
-             <td className='description'> 10' X 4' CROSS BRACES (PURPLE)</td>
+             <td className='description'> 10 X 4 C.BARS (PURPLE)</td>
              <td>{elements.cb10x4 > 0 ? elements.cb10x4 : ""} </td>
              <td></td>
              <td></td>
@@ -175,7 +175,7 @@ function Sheet() {
              <td className='description'> 10 X 2 C.BARS (PINK) </td>
              <td> {elements.cb10x2 > 0 ? elements.cb10x2 : ""} </td>
              <td></td>
-             <td className='description'> 20' O" ALUM BEAMS (PURPLE) </td>
+             <td className='description'> 20'0" ALUM BEAMS (PURPLE) </td>
              <td> {elements.ab20 > 0 ? elements.ab20 : ""} </td>
              <td></td>
           </tr>
@@ -183,7 +183,7 @@ function Sheet() {
              <td className='description'> 7 X 4 C.BARS (YELLOW) </td>
              <td> {elements.cb7x4 > 0 ? elements.cb7x4 : ""} </td>
              <td></td>
-             <td className='description'> 18' O" ALUM BEAMS (RED) </td>
+             <td className='description'> 18'0" ALUM BEAMS (RED) </td>
              <td> {elements.ab18 > 0 ? elements.ab18 : ""} </td>
              <td></td>
           </tr>
@@ -191,7 +191,7 @@ function Sheet() {
              <td className='description'> 7 X 2 C.BARS (RED) </td>
              <td> {elements.cb7x2 > 0 ? elements.cb7x2 : ""} </td>
              <td></td>
-             <td className='description'> 16' O" ALUM BEAMS (YELLOW) </td>
+             <td className='description'> 16'0" ALUM BEAMS (YELLOW) </td>
              <td> {elements.ab16 > 0 ? elements.ab16 : ""} </td>
              <td></td>
           </tr>
@@ -199,7 +199,7 @@ function Sheet() {
              <td className='description'> 5 X 4 C.BARS (GREEN) </td>
              <td> {elements.cb5x4 > 0 ? elements.cb5x4 : ""} </td>
              <td></td>
-             <td className='description'> 14' O" ALUM BEAMS (SILVER) </td>
+             <td className='description'> 14'0" ALUM BEAMS (SILVER) </td>
              <td> {elements.ab14 > 0 ? elements.ab14 : ""} </td>
              <td></td>
           </tr>
@@ -207,7 +207,7 @@ function Sheet() {
              <td className='description'> 5 X 2 C.BARS (ORANGE) </td>
              <td> {elements.cb5x2 > 0 ? elements.cb5x2 : ""} </td>
              <td></td>
-             <td className='description'> 13' O" ALUM BEAMS (PINK) </td>
+             <td className='description'> 13'0" ALUM BEAMS (PINK) </td>
              <td> {elements.ab13 > 0 ? elements.ab13 : ""} </td>
              <td></td>
           </tr>
@@ -215,7 +215,7 @@ function Sheet() {
              <td className='description'> 4 X 4 C.BARS (BLUE) </td>
              <td> {elements.cb4x4 > 0 ? elements.cb4x4 : ""} </td>
              <td></td>
-             <td className='description'> 12' O" ALUM BEAMS (BROWN) </td>
+             <td className='description'> 12'0" ALUM BEAMS (BROWN) </td>
              <td> {elements.ab12 > 0 ? elements.ab12 : ""} </td>
              <td></td>
           </tr>
@@ -223,7 +223,7 @@ function Sheet() {
              <td className='description'> 4 X 2 C.BARS (WHITE) </td>
              <td> {elements.cb4x2 > 0 ? elements.cb4x2 : ""} </td>
              <td></td>
-             <td className='description'> 11' O" ALUM BEAMS (GREEN) </td>
+             <td className='description'> 11'0" ALUM BEAMS (GREEN) </td>
              <td> {elements.ab11 > 0 ? elements.ab11 : ""} </td>
              <td></td>
             </tr>
@@ -231,7 +231,7 @@ function Sheet() {
              <td></td>
              <td></td>
              <td></td>
-             <td className='description'> 10' 6" ALUM BEAMS (BLUE)</td>
+             <td className='description'> 10'6" ALUM BEAMS (BLUE)</td>
              <td> {elements.ab106 > 0 ? elements.ab106 : ""}</td>
              <td></td>
             </tr>
@@ -239,7 +239,7 @@ function Sheet() {
              <td className='description'> ALUM H.D S.J U/HEADS </td>
              <td> {elements.auh > 0 ? elements.auh : ""} </td>
              <td></td>
-             <td className='description'> 10' O" ALUM BEAMS (BLACK) </td>
+             <td className='description'> 10'0" ALUM BEAMS (BLACK) </td>
              <td> {elements.ab10 > 0 ? elements.ab10 : ""} </td>
              <td></td>
             </tr>
@@ -255,7 +255,7 @@ function Sheet() {
             <td className='description'> ALUM REGULAR HEADS </td>
              <td> {elements.auhreg > 0 ? elements.auhreg : ""} </td>
              <td></td>
-             <td className='description'> 8' 0" ALUM BEAMS (RED) </td>
+             <td className='description'> 8'0" ALUM BEAMS (RED) </td>
              <td> {elements.ab8 > 0 ? elements.ab8 : ""} </td>
              <td></td>
             </tr>
@@ -263,7 +263,7 @@ function Sheet() {
              <td className='description'> ALUM H.D. S.J. B/PLATES  </td>
              <td> {elements.abp > 0 ? elements.abp : ""} </td>
              <td></td>
-             <td className='description'> 7' O" ALUM BEAMS (YELLOW) </td>
+             <td className='description'> 7'0" ALUM BEAMS (YELLOW) </td>
              <td> {elements.ab7 > 0 ? elements.ab7 : ""} </td>
              <td></td>
             </tr>
@@ -303,7 +303,7 @@ function Sheet() {
              <td className='description'> STEEL H.D. S.J. U/HEADS 36" </td>
              <td> {elements.suh36 > 0 ? elements.suh36 : ""} </td>
              <td></td>
-             <td className='description'> 14' 0" 4X6 WOOD BEAMS </td>
+             <td className='description'> 14'0" 4X6 WOOD BEAMS </td>
              <td> {elements.wb14 > 0 ? elements.wb14 : ""} </td>
              <td></td>
             </tr>
@@ -320,7 +320,7 @@ function Sheet() {
              <td className='description'> STEEL REGULAR HEADS </td>
              <td> {elements.suhreg > 0 ? elements.suhreg : ""} </td>
              <td></td>
-             <td className='description'> 11' 0" 4X6 WOOD BEAMS </td>
+             <td className='description'> 11'0" 4X6 WOOD BEAMS </td>
              <td> {elements.wb11 > 0 ? elements.wb11 : ""} </td>
              <td></td>
           </tr>
@@ -329,7 +329,7 @@ function Sheet() {
              <td className='description'> STEEL H.D. S.J. B/PLATES </td>
              <td> {elements.sbp > 0 ? elements.sbp : ""} </td>
              <td></td>
-             <td className='description'> 10' 0" 4X6 WOOD BEAMS </td>
+             <td className='description'> 10'0" 4X6 WOOD BEAMS </td>
              <td> {elements.wb10 > 0 ? elements.wb10 : ""} </td>
              <td></td>
           </tr>
@@ -338,7 +338,7 @@ function Sheet() {
              <td className='description'> STEEL H.D. S.J. B/PLATES 36"  </td>
              <td> {elements.sbp36 > 0 ? elements.sbp36 : ""} </td>
              <td></td>
-             <td className='description'> 9' 0" 4X6 WOOD BEAMS </td>
+             <td className='description'> 9'0" 4X6 WOOD BEAMS </td>
              <td> {elements.wb9 > 0 ? elements.wb9 : ""} </td>
              <td></td>
           </tr>
@@ -347,7 +347,7 @@ function Sheet() {
              <td className='description'> STEEL H.D. S.J. B/PLATES 40"  </td>
              <td> {elements.sbp40 > 0 ? elements.sbp40 : ""} </td>
              <td></td>
-             <td className='description'> 8' 0" 4X6 WOOD BEAMS </td>
+             <td className='description'> 8'0" 4X6 WOOD BEAMS </td>
              <td> {elements.wb8 > 0 ? elements.wb8 : ""} </td>
              <td></td>
           </tr>
@@ -395,40 +395,30 @@ function Sheet() {
              <td> {elements.sc4x4x4 > 0 ? elements.sc4x4x4 : ""} </td>
              <td></td>
           </tr>
-
-
-
-
-
-
-
-
-
-            <tr style={{display: elements.extra1 > 0 ? "":"none"}}> 
-             <td className='description'> {elements.extraA}  </td>
-             <td>  {elements.extra1 > 0 ? elements.extra1 : ""} </td>
-             <td></td>
-             <td className='description'> {elements.extraB}  </td>
-             <td>  {elements.extra2 > 0 ? elements.extra2 : ""} </td>
-             <td></td>
-            </tr>
-            <tr style={{display: elements.extra3 > 0 ? "":"none"}}> 
-             <td className='description'> {elements.extraC}  </td>
-             <td>  {elements.extra3 > 0 ? elements.extra3 : ""} </td>
-             <td></td>
-             <td className='description'> {elements.extraD}  </td>
-             <td>  {elements.extra4 > 0 ? elements.extra4 : ""} </td>
-             <td></td>
-            </tr>
-            <tr style={{display: elements.extra5 > 0 ? "":"none"}}> 
-             <td className='description'> {elements.extraE}  </td>
-             <td>  {elements.extra5 > 0 ? elements.extra5 : ""} </td>
-             <td></td>
-             <td className='description'> </td>
-             <td> </td>
-             <td></td>
-            </tr>
-          
+          <tr style={{display: elements.extra1 > 0 ? "":"none"}}> 
+           <td className='description'> {elements.extraA}  </td>
+           <td>  {elements.extra1 > 0 ? elements.extra1 : ""} </td>
+           <td></td>
+           <td className='description'> {elements.extraB}  </td>
+           <td>  {elements.extra2 > 0 ? elements.extra2 : ""} </td>
+           <td></td>
+          </tr>
+          <tr style={{display: elements.extra3 > 0 ? "":"none"}}> 
+           <td className='description'> {elements.extraC}  </td>
+           <td>  {elements.extra3 > 0 ? elements.extra3 : ""} </td>
+           <td></td>
+           <td className='description'> {elements.extraD}  </td>
+           <td>  {elements.extra4 > 0 ? elements.extra4 : ""} </td>
+           <td></td>
+          </tr>
+          <tr style={{display: elements.extra5 > 0 ? "":"none"}}> 
+           <td className='description'> {elements.extraE}  </td>
+           <td>  {elements.extra5 > 0 ? elements.extra5 : ""} </td>
+           <td></td>
+           <td className='description'> </td>
+           <td> </td>
+           <td></td>
+          </tr>
           </tbody>
         </table>
         <p id="totWeight"><b>Total Weight:</b> {totalWeight} lbs</p>
